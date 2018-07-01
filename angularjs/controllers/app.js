55ca8da3c0870be48d51a771118d6819cb6e6e62
romach <romach007@gmail.com>
2018-07-02 00:18:48 +0300

    add fix AngularJS Controller example

--- angularjs/controllers/app.js
+++ angularjs/controllers/app.js
@@ -1,6 +1,12 @@
+// Controllers - use to separate logic from view.
 angular
     .module("app", []) //[3] create module - name, dependencies
-    .controller("UsersController", function ($scope) {  //[4] create controller - $scope as dep. to use vars. in view
+    .controller("UsersController", function ($scope) {  //[4] create controller - $scope as dep. to use variables in view
         $scope.firstName = "John";
         $scope.secondName = "Doe";
+    })
+    .controller("OrdersController", function () {
+        // [5] with "controller as syntax" it is no need to inject $scope to controller, you can refer to variables
+        // using `this`.
+        this.order = "First order";
     });
