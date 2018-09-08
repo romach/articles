1c53be60eeac7b949325250efedafbbb1fb191a1
Roman Cherepanov <romach007@gmail.com>
2018-09-09 00:30:14 +0300

    fix comments

--- angularjs/controllers/app.js
+++ angularjs/controllers/app.js
@@ -1,12 +1,14 @@
 // Controllers - use to separate logic from view.
 angular
-    .module("app", []) //[3] create module - name, dependencies
-    .controller("UsersController", function ($scope) {  //[4] create controller - $scope as dep. to use variables in view
+    //[4] create module. In parameters - name, dependencies
+    .module("app", [])
+    //[5] create controller - pass $scope as dependency to use variables in view
+    .controller("UsersController", function ($scope) {
         $scope.firstName = "John";
         $scope.secondName = "Doe";
     })
     .controller("OrdersController", [function () {
-        // [5] with "controller as syntax" it is no need to inject $scope to controller, you can refer to variables
+        // [6] with "controller as syntax" it is no need to inject $scope to controller, you can refer to variables
         // using `this`.
         this.order = "First order";
     }]);
