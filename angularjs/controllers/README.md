71d946e94851911c4712a3e8e7fe14340a54d61d
Roman Cherepanov <romach007@gmail.com>
2018-11-22 19:01:35 +0200

    Fix AngularJS controllers example

---
+++ angularjs/controllers/README.md
@@ -0,0 +1,3 @@
+This example describe, how to use controllers in AngularJS application.
+
+[**Demo**](https://romach.github.io/examples/angularjs/controllers/)
--- angularjs/controllers/app.js
+++ angularjs/controllers/app.js
@@ -1,14 +1,14 @@
 // Controllers - use to separate logic from view.
 angular
-    //[4] create module. In parameters - name, dependencies
+    //[Hint] create module. In parameters - name, dependencies
     .module("app", [])
-    //[5] create controller - pass $scope as dependency to use variables in view
+    //[Hint] create controller - pass $scope as dependency to use variables in view
     .controller("UsersController", function ($scope) {
         $scope.firstName = "John";
         $scope.secondName = "Doe";
     })
     .controller("OrdersController", [function () {
-        // [6] with "controller as syntax" it is no need to inject $scope to controller, you can refer to variables
+        // [Hint] with "controller as syntax" it is no need to inject $scope to controller, you can refer to variables
         // using `this`.
         this.order = "First order";
     }]);
