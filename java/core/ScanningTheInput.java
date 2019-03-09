04812f903b282ef584d199aed57a47d819d6daf0
Roman Cherepanov <romach007@gmail.com>
2019-03-09 19:51:15 +0200

    Add example with Scanner class

---
+++ java/core/ScanningTheInput.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class ScanningTheInput {
+    public static void main(String[] args) {
+        final Scanner scanner = new Scanner(System.in);
+        String line = scanner.nextLine();
+        System.out.println(line);
+    }
+}
