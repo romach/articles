5d5ac382d520fddd309a9e21874b7ba294743a21
Roman Cherepanov <romach007@gmail.com>
2019-04-24 14:24:59 +0300

    fix JavaScript exceptions example

--- javascript/exceptions/exception.js
+++ javascript/exceptions/exception.js
@@ -1,11 +1,17 @@
 'use strict';
 
+class AgeError extends Error {}
+
 const askAge = () => {
   const answer = prompt("How old are you?");
   if (Number.isInteger(Number.parseFloat(answer))) {
-    return Number.parseInt(answer);
+    const age = Number.parseInt(answer);
+    if (age <= 0) {
+      throw new AgeError("Age can't be less or equal zero!");
+    }
+    return age;
   } else {
-    throw new Error("Invalid age");
+    throw new Error("Wrong input");
   }
 };
 
@@ -13,5 +19,13 @@ try {
   const age = askAge();
   document.getElementsByTagName('body')[0].innerText =`Your age is: ${age}`;
 } catch (error) {
-  document.getElementsByTagName('body')[0].innerText =`${error}`;
+  // print error and error stack trace if error is AgeError. Otherwise, rethrow error
+  if (error instanceof AgeError) {
+    document.getElementsByTagName('body')[0].innerText =`${error}\n${error.stack}`;
+  } else {
+    throw error;
+  }
+} finally {
+  // this block is executed in any case
+  document.getElementsByTagName('body')[0].innerText +=`\nAge was prompted!`;
 }
