21ee39b6366b1964eefa3bcec4ffc3485de0f4b5
Roman Cherepanov <romach007@gmail.com>
2020-05-05 18:34:01 +0300

    #55: fix applications

--- react/state/initial/src/App.js
+++ react/state/initial/src/App.js
@@ -1,21 +1,13 @@
 import React from "react";
 import "./tailwind.min.css";
+import UserItem from "./components/UserItem";
+import users from "./users.json";
 
 function App() {
   return (
     <div className="bg-gray-200 flex">
       <div className="mx-auto" style={{width: 600}}>
-        <div className="flex bg-white rounded-lg p-6 m-6 shadow-md">
-          <div>
-            <img className="h-24 w-24 mx-0 mr-6 rounded-full" src="avatar-1.jpeg" />
-          </div>
-          <div className="text-left">
-            <h2 className="text-lg">Scott Reynolds</h2>
-            <div className="text-purple-500">Customer Support</div>
-            <div className="text-gray-600">scottreynolds@example.com</div>
-            <div className="text-gray-600">(555) 765-4321</div>
-          </div>
-        </div>
+        {users.map(user => <UserItem key={user.email} user={user}/>)}
       </div>
     </div>
   );
