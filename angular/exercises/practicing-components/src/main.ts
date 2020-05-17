d6c95b6b82be4fe060b13a234c102d2066197673
Roman Cherepanov <romach007@gmail.com>
2020-05-17 19:02:48 +0300

    try to fix codesandbox

--- angular/exercises/practicing-components/src/main.ts
+++ angular/exercises/practicing-components/src/main.ts
@@ -4,6 +4,10 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+import 'core-js/es/reflect';
+import 'core-js/stable/reflect';
+import 'core-js/features/reflect';
+
 if (environment.production) {
   enableProdMode();
 }
