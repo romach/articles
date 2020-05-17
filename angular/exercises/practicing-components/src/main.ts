3f7428bcf74bd83fea4405bff4eded6b98a6e99c
Roman Cherepanov <romach007@gmail.com>
2020-05-17 18:14:35 +0300

    Try to fix bug with running in codesandbox

--- angular/exercises/practicing-components/src/main.ts
+++ angular/exercises/practicing-components/src/main.ts
@@ -3,6 +3,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+import "core-js/proposals/reflect-metadata";
 
 if (environment.production) {
   enableProdMode();
